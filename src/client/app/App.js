8d53e19f7f5903c822d8030d3bd862607c61edbf
Vishal Lakkapathri <Vishal.Lakkapathri@copart.com>
2017-09-20 19:47:22 +0530

    BoilerPlate Code for React

---
+++ src/client/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    console.log('Main app render fn');
+    return <div>
+
+    </div>
+  }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      }
+    }
+  ]
+},
+devServer: {
+     contentBase: './src/client/public',
+   }
+};
+
+module.exports = config;
